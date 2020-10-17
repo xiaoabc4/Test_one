024c3beb30f134375ec4b7e46b859564bfd3c2bf
xiaoabc4 <64999450+xiaoabc4@users.noreply.github.com>
2020-10-17 21:31:38 +0800

    3x3

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,5 +1,8 @@
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("3333");
+        System.out.println("3333");
+        System.out.println("3333");
+        System.out.println("3333");
     }
 }
